1dd93a282f79849920bd0de90c77809ede87deb1
Martin Hilton <martin.hilton@canonical.com>
2019-04-05 08:57:05 +0100

    support http_proxy

--- README.md
+++ README.md
@@ -6,7 +6,17 @@ The [Athens](https://docs.gomods.io/) go module proxy.
 Configuration
 -------------
 
+These are the configuration settings that affect the athens server. Some
+are taken from lower layers.
+
 `athens-image` specifies the docker image that the charm should install.
 
-`netrc` specifies a base64 encoded netrc file that athens will use to
-authenticate http requests.
+`http_proxy` specifies the HTTP proxy used by athens.
+
+`https_proxy` specifies the HTTPS proxy used by athens.
+
+`netrc` specifies a base64 encoded netrc file that athens (go and git)
+will use to authenticate http requests.
+
+`no_proxy` specifies hosts that will bypass the specified proxy in
+HTTP requests.
--- reactive/athens.py
+++ reactive/athens.py
@@ -12,52 +12,64 @@ from charmhelpers.core.hookenv import (
 )
 from charmhelpers.core.host import write_file
 from charms.docker import Compose
-from charms.reactive import set_flag, when, when_file_changed
-
-
-@when('config.changed')
-def config_changed():
-    env = [
-        'ATHENS_STORAGE_TYPE=disk',
-        'ATHENS_DISK_STORAGE_ROOT=/srv/athens/cache']
-    if os.path.exists('/srv/athens/netrc'):
-        env.append('ATHENS_NETRC_PATH=/srv/athens/netrc')
-    write_file('/srv/athens/env', "\n".join(env).encode('utf-8'))
+from charms.reactive import (
+    clear_flag,
+    set_flag,
+    when,
+    when_any,
+    when_file_changed,
+    when_not,
+)
 
 
 @when('docker.available')
-def athens_up():
-    status_set('maintenance', 'docker-compose up athens')
-    os.environ['athens_image'] = config()['athens-image']
-    Compose(charm_dir()).up()
-    set_flag("athens.up")
-    application_version_set(
-        requests.get('http://localhost:3000/version').json()['version'])
-    status_set('active', 'athens running')
+@when_not('athens.up')
+def start():
+    compose()
+
+
+@when_file_changed(os.path.join(charm_dir(), 'docker-compose.yaml'))
+def docker_compose_changed():
+    set_flag('athens.restart')
 
 
 @when('athens.up')
+@when_not('athens.port.open')
 def config_port():
     open_port(3000)
+    set_flag('athens.port.open')
 
 
-@when('athens.up')
-@when_file_changed('/srv/athens/env', '/srv/athens/netrc')
-def athens_config_changed():
-    athens_up()
-
-
-@when('athens.up')
-@when('config.changed.athens-image')
-def athens_image_changed():
-    athens_up()
+@when_any(
+    'config.changed.athens-image',
+    'config.changed.http_proxy',
+    'config.changed.https_proxy',
+    'config.changed.no_proxy')
+def config_changed():
+    set_flag('athens.restart')
 
 
 @when('config.changed.netrc')
-def netrc():
-    v = config()['netrc']
-    if v == "" and os.path.exists('/srv/athens/netrc'):
-        os.remove('/srv/athens/netrc')
-    elif v != "":
-        write_file('/srv/athens/netrc', base64.b64decode(v))
-    config_changed()
+def config_changed_netrc():
+    write_file('/srv/athens/netrc', base64.b64decode(config()['netrc']))
+    set_flag('athens.restart')
+
+
+@when('athens.up')
+@when('athens.restart')
+def compose():
+    '''compose brings up the athens service.'''
+    status_set('maintenance', 'docker-compose up athens')
+    os.environ['athens_image'] = config()['athens-image']
+    os.environ['ATHENS_DISK_STORAGE_ROOT'] = '/srv/athens/cache'
+    os.environ['ATHENS_STORAGE_TYPE'] = 'disk'
+    os.environ['ATHENS_NETRC_PATH'] = '/srv/athens/netrc'
+    os.environ['http_proxy'] = config()['http_proxy']
+    os.environ['https_proxy'] = config()['https_proxy']
+    os.environ['no_proxy'] = config()['no_proxy']
+    Compose(charm_dir()).up()
+    clear_flag('athens.restart')
+    set_flag('athens.up')
+    application_version_set(
+        requests.get('http://localhost:3000/version').json()['version'])
+    status_set('active', 'athens running')
